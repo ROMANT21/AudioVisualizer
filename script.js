009e6056d876e3c758b1be3d76af30536ba53d73
ROMANT21 <tyler73750@gmail.com>
2022-09-17 20:21:52 -0700

    Finished tutoria; bar chart for project

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
-// Create audio element set to location of audio file
-let audio1 = new Audio();
-audio1.src = 'tune.mp3';
+const button1 = document.getElementById('button1');
+let audio1 = new Audio('The Beatles - Come Together.mp3');
+const audioContext = new AudioContext();
 
-// Gather container and canvas elements from html file
 const container = document.getElementById('container');
-const canvas = document.getElementById('canvas');
-
-// Set canvas element equal to browser window
+const canvas = document.getElementById('canvas1');
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.Height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+let audioSource;
+let analyser
 
-// Needed to create audio source & analyzer
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-let audioSource = null;
-let analyser = null;
+container.addEventListener('click', function(){
+    audio1.play();
+    audioSource = audioContext.createMediaElementSource(audio1);
+    analyser = audioContext.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
 
-audio1.play();
 
-// Create audio source & analyzer nodes
-audioSource = audioCtx.createMediaElementSource(audio1);
-analyser = audioCtx.createAnalyser();
-// Create chain for audio to flow through
-audioSource.connect(analyser); // Connect audio to anaylyzer
-analyser.connect(audioCtx.destination); // Connect analyzer to speakers
-
-analyser.fftSize = 128; // samples (data) per second
-const bufferLength = analyser.frequencyBinCount; // Defined as: fftsize / 2
-const dataArray = new Uint8Array(bufferLength); // Hold all data points collected from sound
-const barWidth = canvas.width / bufferLength; // Determine size of bars across screen
-
-function animate() {
-  x = 0;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  analyser.getByteFrequencyData(dataArray);
-  for (let i = 0; i < bufferLength; ++i) {
-    barHeight = dataArray[i];
-    ctx.fillStyle = 'white';
-    ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-    x += barWidth;
-  }
-
-  requestAnimationFrame(animate);
-}
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x;
+    
+    function animate(){
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        for (let i  = 0; i < bufferLength; i++){
+            barHeight = dataArray[i] / 2;
+            ctx.fillStyle = 'white';
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth;
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
